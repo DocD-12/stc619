987e8437209162b906007fd196332b536dd82a5d
Artem Davletshin <artemdavletshin12@gmail.com>
2025-01-31 22:54:42 +0300

    init main_skyfield

---
+++ main_skyfield.py
@@ -0,0 +1,27 @@
+from skyfield.api import load
+from skyfield.sgp4lib import EarthSatellite
+from skyfield.toposlib import wgs84
+from datetime import timedelta
+
+ts = load.timescale()
+t = ts.now()
+t0 = t
+t1 = t0 + timedelta(days=7)
+
+l1 = '1 57202U 23091AN  25031.15474867  .00008229  00000+0  47874-3 0  9997'
+l2 = '2 57202  97.5855  87.8579 0011980  52.4663 307.7654 15.12253614 87902'
+sl = EarthSatellite(l1, l2, 'CSTP 1.1 (STC 1.1)', ts)
+
+lat, lon = 59.983833, 30.394778
+stp = wgs84.latlon(lat, lon)
+dif = sl - stp
+
+t2, ev = sl.find_events(stp, t0, t1, altitude_degrees=30)
+
+for ti, event in zip(t2, ev):
+    if event == 1:
+        tm = ti.utc_strftime('%Y %b %d %H:%M:%S')
+        top = dif.at(ti)
+        alt, az, dist = top.altaz()
+#        alt, az = alt.degrees, az.degrees
+        print(f'Время кульминации сеанса:{tm}\n\tУгол над горизонтом: {alt}\n\tАзимут: {az}')
