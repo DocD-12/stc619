63aae3d09b36c23b0df69560adbb4e9534da0c38
davletshin_a_F <artemdavletshin12@gmail.com>
2025-02-20 18:28:49 +0300

    divided two main.py

--- main_qt.py
+++ main_qt.py
@@ -1,116 +1,68 @@
 import sys
-
-from PyQt6 import QtCore, QtGui, QtWidgets
-
-SCALE_FACTOR = 1.25
+from PyQt6 import QtCore, QtWidgets, QtGui
 
 
-class PhotoViewer(QtWidgets.QGraphicsView):
-    coordinatesChanged = QtCore.pyqtSignal(QtCore.QPoint)
-    def __init__(self, parent):
-        super().__init__(parent)
-        self._scene = QtWidgets.QGraphicsScene(self)
-        self._photo = QtWidgets.QGraphicsPixmapItem()
-        self._scene.addItem(self._photo)
-        self.setScene(self._scene)
-        self.setTransformationAnchor(
-            QtWidgets.QGraphicsView.ViewportAnchor.AnchorUnderMouse)
-        self.setResizeAnchor(
-            QtWidgets.QGraphicsView.ViewportAnchor.AnchorUnderMouse)
-        self.setVerticalScrollBarPolicy(
-            QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.setHorizontalScrollBarPolicy(
-            QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(30, 30, 30)))
-        self.setFrameShape(QtWidgets.QFrame.Shape.NoFrame)
-
-    def resetView(self, scale=1):
-        rect = QtCore.QRectF(self._photo.pixmap().rect())
-        if not rect.isNull():
-            self.setSceneRect(rect)
-            if (scale := max(1, scale)) == 1:
-                self._zoom = 0
-            unity = self.transform().mapRect(QtCore.QRectF(0, 0, 1, 1))
-            self.scale(1 / unity.width(), 1 / unity.height())
-            viewrect = self.viewport().rect()
-            scenerect = self.transform().mapRect(rect)
-            factor = min(viewrect.width() / scenerect.width(),
-                         viewrect.height() / scenerect.height()) * scale
-            self.scale(factor, factor)
-            self.updateCoordinates()
-
-    def Photo(self):
-        self.setDragMode(QtWidgets.QGraphicsView.DragMode.ScrollHandDrag)
-        self._photo.setPixmap(QtGui.QPixmap("Worldmap_grid.png"))
-        self._zoom = 0
-        self.resetView(SCALE_FACTOR ** float(self._zoom))
-
-    def zoom(self, step):
-        zoom = max(0, self._zoom + (step := int(step)))
-        if zoom != self._zoom:
-            self._zoom = zoom
-            if self._zoom > 0:
-                if step > 0:
-                    factor = SCALE_FACTOR ** step
-                else:
-                    factor = 1 / SCALE_FACTOR ** abs(step)
-                self.scale(factor, factor)
-            else:
-                self.resetView()
-
-    def wheelEvent(self, event):
-        delta = event.angleDelta().y()
-        self.zoom(delta and delta // abs(delta))
-
-    def resizeEvent(self, event):
-        super().resizeEvent(event)
-        self.resetView()
-
-    def updateCoordinates(self, pos=None):
-        if self._photo.isUnderMouse():
-            if pos is None:
-                pos = self.mapFromGlobal(QtGui.QCursor.pos())
-            point = self.mapToScene(pos).toPoint()
-        else:
-            point = QtCore.QPoint()
-        self.coordinatesChanged.emit(point)
-
-    def mouseMoveEvent(self, event):
-        self.updateCoordinates(event.position().toPoint())
-        super().mouseMoveEvent(event)
-
-    def leaveEvent(self, event):
-        self.coordinatesChanged.emit(QtCore.QPoint())
-        super().leaveEvent(event)
-
-
-class Window(QtWidgets.QWidget):
+class MainGraphicView(QtWidgets.QGraphicsView):
     def __init__(self):
         super().__init__()
-        self.showFullScreen()
-        self.viewer = PhotoViewer(self)
-        self.viewer.coordinatesChanged.connect(self.handleCoords)
-        self.labelCoords = QtWidgets.QLabel(self)
-        self.labelCoords.setAlignment(
-            QtCore.Qt.AlignmentFlag.AlignRight |
-            QtCore.Qt.AlignmentFlag.AlignCenter)
-        layout = QtWidgets.QGridLayout(self)
-        layout.addWidget(self.viewer, 0, 0, 1, 3)
-        layout.addWidget(self.labelCoords, 1, 2, 1, 1)
-        layout.setColumnStretch(2, 2)
-        self.labelCoords.clear()
-        self.viewer.Photo()
+        self.scene = QtWidgets.QGraphicsScene()
+        self.scene.addPixmap(QtGui.QPixmap("Worldmap_grid.png"))
+        self.setScene(self.scene)
 
-    def handleCoords(self, point):
-        if not point.isNull():
-            self.labelCoords.setText(f'{point.x()}, {point.y()}')
-        else:
-            self.labelCoords.clear()
+        self.x0 = 39
+        self.x1 = 2520
+        self.y0 = 19
+        self.y1 = 1263
+
+        self.pic_sat = QtWidgets.QGraphicsPixmapItem()
+        self.pic_sat.setPixmap(QtGui.QPixmap('sat.png').scaled(50, 50))
+        self.scene.addItem(self.pic_sat)
+
+        self.pic_base = QtWidgets.QGraphicsPixmapItem()
+        self.pic_base.setPixmap(QtGui.QPixmap('base.png').scaled(50, 50))
+        self.scene.addItem(self.pic_base)
+
+        self.scene.installEventFilter(self)
+        self.setMouseTracking(True)
 
 
-if __name__ == '__main__':
+    def eventFilter(self, source, event):
+        if event.type() == QtCore.QEvent.Type.GraphicsSceneMouseRelease:
+            item = self.scene.itemAt(event.scenePos(), QtGui.QTransform())
+            if isinstance(item, QtWidgets.QGraphicsPixmapItem):
+                # map the scene position to item coordinates
+                map = item.mapFromScene(event.scenePos())
+                # print(f'mouse is on pixmap at coordinates {map.x()}, {map.y()}')
+                geocoo = self.pixtoGeo(map.x(), map.y())
+                print(f'mouse is on pixmap at coordinates {geocoo}')
+                self.pic_base.setPos(map.x() - 25, map.y() - 25)
+
+        return super().eventFilter(source, event)
+
+    def moveSatto(self, lon, lat):
+        pass
+
+    def geotoPix(self, lon, lat):
+        pass
+
+    def pixtoGeo(self, x, y):
+        return x, y
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        w = QtWidgets.QWidget()
+        l = QtWidgets.QVBoxLayout()
+        l.addWidget(MainGraphicView())
+        w.setLayout(l)
+
+        self.setCentralWidget(w)
+
+
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = Window()
-    window.setGeometry(0, 0, 1920, 1080)
+    window = MainWindow()
     window.show()
-    sys.exit(app.exec())
+
+    app.exec()
---
+++ not_main.py
@@ -0,0 +1,116 @@
+import sys
+
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+SCALE_FACTOR = 1.25
+
+
+class PhotoViewer(QtWidgets.QGraphicsView):
+    coordinatesChanged = QtCore.pyqtSignal(QtCore.QPoint)
+    def __init__(self, parent):
+        super().__init__(parent)
+        self._scene = QtWidgets.QGraphicsScene(self)
+        self._photo = QtWidgets.QGraphicsPixmapItem()
+        self._scene.addItem(self._photo)
+        self.setScene(self._scene)
+        self.setTransformationAnchor(
+            QtWidgets.QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        self.setResizeAnchor(
+            QtWidgets.QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(
+            QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(
+            QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(30, 30, 30)))
+        self.setFrameShape(QtWidgets.QFrame.Shape.NoFrame)
+
+    def resetView(self, scale=1):
+        rect = QtCore.QRectF(self._photo.pixmap().rect())
+        if not rect.isNull():
+            self.setSceneRect(rect)
+            if (scale := max(1, scale)) == 1:
+                self._zoom = 0
+            unity = self.transform().mapRect(QtCore.QRectF(0, 0, 1, 1))
+            self.scale(1 / unity.width(), 1 / unity.height())
+            viewrect = self.viewport().rect()
+            scenerect = self.transform().mapRect(rect)
+            factor = min(viewrect.width() / scenerect.width(),
+                         viewrect.height() / scenerect.height()) * scale
+            self.scale(factor, factor)
+            self.updateCoordinates()
+
+    def Photo(self):
+        self.setDragMode(QtWidgets.QGraphicsView.DragMode.ScrollHandDrag)
+        self._photo.setPixmap(QtGui.QPixmap("Worldmap_grid.png"))
+        self._zoom = 0
+        self.resetView(SCALE_FACTOR ** float(self._zoom))
+
+    def zoom(self, step):
+        zoom = max(0, self._zoom + (step := int(step)))
+        if zoom != self._zoom:
+            self._zoom = zoom
+            if self._zoom > 0:
+                if step > 0:
+                    factor = SCALE_FACTOR ** step
+                else:
+                    factor = 1 / SCALE_FACTOR ** abs(step)
+                self.scale(factor, factor)
+            else:
+                self.resetView()
+
+    def wheelEvent(self, event):
+        delta = event.angleDelta().y()
+        self.zoom(delta and delta // abs(delta))
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.resetView()
+
+    def updateCoordinates(self, pos=None):
+        if self._photo.isUnderMouse():
+            if pos is None:
+                pos = self.mapFromGlobal(QtGui.QCursor.pos())
+            point = self.mapToScene(pos).toPoint()
+        else:
+            point = QtCore.QPoint()
+        self.coordinatesChanged.emit(point)
+
+    def mouseMoveEvent(self, event):
+        self.updateCoordinates(event.position().toPoint())
+        super().mouseMoveEvent(event)
+
+    def leaveEvent(self, event):
+        self.coordinatesChanged.emit(QtCore.QPoint())
+        super().leaveEvent(event)
+
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.showFullScreen()
+        self.viewer = PhotoViewer(self)
+        self.viewer.coordinatesChanged.connect(self.handleCoords)
+        self.labelCoords = QtWidgets.QLabel(self)
+        self.labelCoords.setAlignment(
+            QtCore.Qt.AlignmentFlag.AlignRight |
+            QtCore.Qt.AlignmentFlag.AlignCenter)
+        layout = QtWidgets.QGridLayout(self)
+        layout.addWidget(self.viewer, 0, 0, 1, 3)
+        layout.addWidget(self.labelCoords, 1, 2, 1, 1)
+        layout.setColumnStretch(2, 2)
+        self.labelCoords.clear()
+        self.viewer.Photo()
+
+    def handleCoords(self, point):
+        if not point.isNull():
+            self.labelCoords.setText(f'{point.x()}, {point.y()}')
+        else:
+            self.labelCoords.clear()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window()
+    window.setGeometry(0, 0, 1920, 1080)
+    window.show()
+    sys.exit(app.exec())
